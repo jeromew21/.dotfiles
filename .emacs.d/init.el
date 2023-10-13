e3a4c7dadf8bbf464e55ad1c749e912d4e42e8a6
Jerome Wei <jeromew@berkeley.edu>
2023-10-13 12:23:27 -0700

    Added emacs init

---
+++ .emacs.d/init.el
@@ -0,0 +1,80 @@
+(setq-default indent-tabs-mode nil
+              tab-width 4
+              tabs-width 4)
+
+(setq indent-line-function 'insert-tab)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+         
+(tool-bar-mode -1)
+
+(set-frame-font "Inconsolata Nerd Font 16" nil t)
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'catppuccin-theme)
+  (package-install 'catppuccin-theme))
+
+(unless (package-installed-p 'neotree)
+  (package-install 'neotree))
+
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; LSP config
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
+    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+(which-key-mode)
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+;; end LSP config
+
+
+(require 'evil)
+(evil-mode 1)
+
+(load-theme 'catppuccin :no-confirm)
+
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(neotree catppuccin-theme evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
