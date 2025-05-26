c4d63197d93bad300ca45bee79903519af361109
Jerome Wei <jeromew@berkeley.edu>
2025-05-25 22:20:25 -0400

    Added shell for C++ LSP.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,77 +1,99 @@
-(setq-default indent-tabs-mode nil
-              tab-width 4
-              tabs-width 4)
+;; Speed up startup
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
 
-(setq indent-line-function 'insert-tab)
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode)
-         
 (tool-bar-mode -1)
 
-(set-frame-font "Inconsolata Nerd Font 16" nil t)
-
-;; Set up package.el to work with MELPA
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                           ("gnu" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
-(package-refresh-contents)
+  (unless package-archive-contents
+    (package-refresh-contents))
 
-(unless (package-installed-p 'catppuccin-theme)
-  (package-install 'catppuccin-theme))
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-(unless (package-installed-p 'neotree)
-  (package-install 'neotree))
+;; Project management
+(use-package projectile
+  :config (projectile-mode 1))
 
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+;; Auto-completion
+(use-package company
+  :config (global-company-mode))
+  
+;; Snippets
+(use-package yasnippet
+  :config (yas-global-mode 1))
+  
+;; LSP support
+(use-package lsp-mode
+  :hook ((c++-mode . lsp)
+         (c-mode . lsp))
+  :commands lsp
+  :config
+  (setq lsp-prefer-capf t)
+  (setq lsp-enable-snippet t))
+  
+;; LSP UI
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-sideline-show-code-actions t
+        lsp-ui-doc-enable t))
+  
+;; Treemacs integration with LSP
+(use-package lsp-treemacs
+  :after lsp)
+  
+;; DAP mode (Debugging)
+(use-package dap-mode
+  :after lsp-mode
+  :config
+  (dap-auto-configure-mode)
+  (require 'dap-lldb))
+  
+;; File explorer
+(use-package treemacs)
+  
+;; Git integration
+(use-package magit)
+  
+;; CMake mode
+(use-package cmake-mode)
+  
+;; Clang-format integration
+(use-package clang-format
+  :hook ((c-mode . (lambda () (add-hook 'before-save-hook #'clang-format-buffer nil t)))
+         (c++-mode . (lambda () (add-hook 'before-save-hook #'clang-format-buffer nil t))))
+  :config (setq clang-format-style-option "llvm"))
+  
+;; Keybinding help
+(use-package which-key
+  :config (which-key-mode))
+  
+;; Linting
+(use-package flycheck
+ :init (global-flycheck-mode))
 
-;; LSP config
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
-    projectile hydra flycheck company avy which-key helm-xref dap-mode))
-
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
-
-;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
-(helm-mode)
-(require 'helm-xref)
-(define-key global-map [remap execute-extended-command] #'helm-M-x)
-(define-key global-map [remap switch-to-buffer] #'helm-mini)
-
-(which-key-mode)
-(add-hook 'c-mode-hook 'lsp)
-(add-hook 'c++-mode-hook 'lsp)
-
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-idle-delay 0.0
-      company-minimum-prefix-length 1
-      lsp-idle-delay 0.1)  ;; clangd is fast
-
-(with-eval-after-load 'lsp-mode
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (require 'dap-cpptools)
-  (yas-global-mode))
-;; end LSP config
-
-
-(require 'evil)
-(evil-mode 1)
-
+;; Catpuccin theme
+(use-package catppuccin-theme)
 (load-theme 'catppuccin :no-confirm)
 
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-
+;; Optional: Vim keybindings
+(use-package evil
+  :config (evil-mode 1))
+  
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(neotree catppuccin-theme evil)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
